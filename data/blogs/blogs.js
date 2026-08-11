47e669d05949199d532f06bb7fc1d81175abd67f
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2026-08-11 22:11:07 +0600

    feat: blog item

--- data/blogs/blogs.js
+++ data/blogs/blogs.js
@@ -1,7 +1,18 @@
 export const blogsData = [
   {
     id: 0,
-    title: "Deploying a Full-Stack App on AWS EC2 with Nginx Load Balancer (Frontend & Backend)",
+    title: "AWS Networking: From Packet to VPC",
+    href: "https://medium.com/towardsdev/aws-networking-from-packet-to-vpc-6319aa509430",
+    subtitle:
+      "When I first started working with AWS, I could launch an EC2 instance inside a VPC, attach a security group, and deploy an...",
+    cover_image: "/images/blogs/aws-infra.jpeg",
+    date: "Aug 11, 2026",
+    tags: ["AWS", "EC2", "VPC", "Subnet", "Networking"],
+  },
+  {
+    id: 1,
+    title:
+      "Deploying a Full-Stack App on AWS EC2 with Nginx Load Balancer (Frontend & Backend)",
     href: "https://medium.com/towardsdev/deploying-a-full-stack-app-on-aws-ec2-with-nginx-load-balancer-frontend-backend-on-separate-afaf910bb106",
     subtitle: "I'll walk through how I deployed the frontend...",
     cover_image: "/images/blogs/aws-deployment.webp",
@@ -9,7 +20,7 @@ export const blogsData = [
     tags: ["AWS", "EC2", "Nginx", "DevOps"],
   },
   {
-    id: 1,
+    id: 2,
     title: "JavaScript Asynchronous Behavior (Part 1) - Callbacks in Depth",
     href: "https://medium.com/dev-genius/javascript-asynchronous-behavior-part-1-callbacks-in-depth-ca33fb8dcd04",
     subtitle: "A callback function is a function passed into...",
@@ -18,7 +29,18 @@ export const blogsData = [
     tags: ["JavaScript", "Async", "Callbacks"],
   },
   {
-    id: 2,
+    id: 3,
+    title:
+      "Performance Optimization and Get Child State without Re-Rendering..",
+    href: "https://medium.com/@rafiurrahmanprotik/performance-optimization-and-get-child-state-without-re-rendering-the-child-component-using-useref-d649ab7db960",
+    subtitle: 'When we talk about "re-rendering...',
+    cover_image:
+      "https://miro.medium.com/v2/resize:fit:750/format:webp/0*3IJaEsj63N8A8fKV.png",
+    date: "Nov 19, 2023",
+    tags: ["React.js", "useRef", "Performance"],
+  },
+  {
+    id: 4,
     title: "Crud Operation with Array and Object in JavaScript.",
     href: "https://towardsdev.com/crud-operation-with-array-and-object-in-javascript-56af95aff3b2",
     subtitle: "In this article I'm going to show you how to..",
@@ -27,18 +49,9 @@ export const blogsData = [
     date: "Feb 8, 2022",
     tags: ["JavaScript", "Arrays", "Objects"],
   },
+
   {
-    id: 3,
-    title: "Performance Optimization and Get Child State without Re-Rendering..",
-    href: "https://medium.com/@rafiurrahmanprotik/performance-optimization-and-get-child-state-without-re-rendering-the-child-component-using-useref-d649ab7db960",
-    subtitle: "When we talk about \"re-rendering...",
-    cover_image:
-      "https://miro.medium.com/v2/resize:fit:750/format:webp/0*3IJaEsj63N8A8fKV.png",
-    date: "Nov 19, 2023",
-    tags: ["React.js", "useRef", "Performance"],
-  },
-  {
-    id: 4,
+    id: 5,
     title: "Express.js Project Structure and Middleware Concept for Beginners",
     href: "https://medium.com/@rafiurrahmanprotik/express-js-project-structure-and-middleware-concept-for-beginners-ff7bb0baea35",
     subtitle: "Express.js is one of the most popular backend frameworks...",
